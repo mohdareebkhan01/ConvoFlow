3ae80f3e90d0d7e0e9aac4c315f4f832612e605f
Mohd Areeb <khanaareeb246@gmail.com>
2026-07-27 15:26:59 +0530

    signup controller added

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,17 +1,38 @@
 // const express = require("express");
 import express from "express";
 import dotenv from "dotenv";
+import path from "path";
 
 import authRoutes from "./routes/auth.route.js";
 import messageRoutes from "./routes/message.route.js";
+import {connectDB} from "./lib/db.js";
 
 dotenv.config();
 
 const app = express();
 
+const __dirname = path.resolve();
+
 const PORT = process.env.PORT || 3000;
+
+app.use(express.json());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 
-app.listen(PORT,() => console.log("Server running on port: " + PORT));
+//make ready for deployement
+ if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/dist")))
+
+    app.get("/{*splat}", (req, res) => {
+    res.sendFile(
+        path.join(__dirname, "../../frontend/dist/index.html")
+    );
+});
+}
+ 
+
+app.listen(PORT,() => {
+    console.log("Server running on port: " + PORT)
+     connectDB();
+});
