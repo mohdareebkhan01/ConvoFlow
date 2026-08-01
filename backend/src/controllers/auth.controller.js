50fa1c298b5ab26b5fc310297b1a1a26a81cb89f
Mohd Areeb <khanaareeb246@gmail.com>
2026-08-01 17:14:51 +0530

    ProfileUpdateImplemented

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -3,6 +3,7 @@ import {generateToken}  from "../lib/utils.js";
 import User from "../models/User.js"
 import bcrypt from "bcryptjs"
 import {ENV} from "../lib/env.js";
+import cloudiany from "../lib/cloudinary.js "
 
 export const signup = async (req,res) => {
     const {fullName, email, password} = req.body;
@@ -57,3 +58,48 @@ export const signup = async (req,res) => {
        res.status(500).json({message:"Internal server error"})
     }
 };  
+
+export const login = async(req,res) => {
+  const {email,password} = req.body;
+  try{
+    const user = await User.findOne({email : email})
+    if(!user) return res.status(400).json({message : "Invalid Credentials"});
+      //never tell the client which one is incorrect. 
+      const isPasswordCorrect = await bcrypt.compare(password, user.password);
+      if(!isPasswordCorrect) return res.status(400).json({message : "Invalid credentials"});
+      
+      generateToken(user._id,res);
+      res.status(200).json({
+        _id: user._id,
+        fullName: user.fullName,
+        email : user.email,
+        profilePic : user.profilePic,
+      });
+  } catch(error) {
+      console.error("Error in login credentials:" , error);
+      res.status(500).json({message : "Internal server error"}); 
+  }
+};
+
+export const logout = async(req,res) => {
+  res.cookie("jwt","", {maxAge : 0});
+  res.status(200).json({ message : "Logged out successfully"});
+};
+
+export const updateProfile = async(req,res) => {
+  try{
+    const {profilePic} = req.body;
+    if(!profilePic) return res.status(400).json({message: "Profile pic is required"})
+    const userId = req.user._id;
+    const uploadResponse = await cloudinary.uploader.upload(profilePic);
+    const updateUser = await User.findByIdAndUpdate(
+      userId,
+      {profilePic: uplodadResponse.secure_url},
+      {new:true}
+    );
+    res.status(200).json(updatedUser);
+  } catch(error) {
+    console.log("Error in update profile:" , error);
+    res.status(500).json({message: "Internal server error"});
+  }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -2,6 +2,7 @@
 import express from "express";
 import dotenv from "dotenv";
 import path from "path";
+import cookieParser from "cookie-parser";
 import {ENV} from "./lib/env.js";
 
 import authRoutes from "./routes/auth.route.js";
@@ -17,6 +18,7 @@ const __dirname = path.resolve();
 const PORT = ENV.PORT || 3000;
 
 app.use(express.json());
+app.use(cookieParser());
 
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
