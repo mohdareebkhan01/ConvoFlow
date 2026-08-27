bd15360bf4090ccec20ddfbf2c6968b129aafcc3
Mohd Areeb <khanaareeb246@gmail.com>
2026-08-27 21:51:16 +0530

    Backend url updated

--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,6 +1,8 @@
 import axios from "axios"
 
 export const axiosInstance = axios.create({
-    baseURL: import.meta.env.MODE === "development" ? "http://localhost:5000/api" : "/api",
+    baseURL: import.meta.env.MODE === "development"
+        ? "http://localhost:5000/api"
+        : "https://convoflow-5c3u.onrender.com/api",
     withCredentials: true,
 })
